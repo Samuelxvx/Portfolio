84365c96176f2a86b5e6c3369bc81156c8d39ca4
Samuelxvx <tanakirt.takatoku@gmail.com>
2023-01-18 13:03:45 +0900

    new changes

---
+++ app.js
@@ -0,0 +1,59 @@
+const trailer = document.getElementById("trailer");
+
+const animateTrailer = (e, interacting) => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+        y = e.clientY - trailer.offsetHeight / 2;
+  
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 4.5 : 1})`
+  }
+  
+  trailer.animate(keyframes, { 
+    duration: 800, 
+    fill: "forwards" 
+  });
+}
+
+const getTrailerClass = type => {
+  switch(type) {
+    case "video":
+      return "fa-solid fa-play";
+    case "link":
+      return "fa-regular fa-eye";
+    default:
+      return "fa-regular fa-hand-pointer"; 
+  }
+}
+
+window.onmousemove = e => {
+  const interactable = e.target.closest(".interactable"),
+        interacting = interactable !== null;
+  
+  const icon = document.getElementById("trailer-icon");
+  
+  animateTrailer(e, interacting);
+  
+  trailer.dataset.type = interacting ? interactable.dataset.type : "";
+  
+  if(interacting) {
+    icon.className = getTrailerClass(interactable.dataset.type);
+  }
+}
+function hideMenu() {
+  let menuOpen = document.querySelector(".toggler").checked;
+
+  if ((menuOpen = true)) {
+    document.querySelector(".toggler").checked = false;
+  }
+}
+window.addEventListener("scroll", hideMenu);
+
+
+const links = document.querySelectorAll("a.text-link");
+const checkbox = document.querySelector("input[type='checkbox']");
+
+links.forEach(link => {
+    link.addEventListener("click", function() {
+        checkbox.checked = false;
+    });
+});
