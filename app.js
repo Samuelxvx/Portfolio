639816d218ca1b9e7cbcbb5469a01038a167ca38
Samuelxvx <tanakirt.takatoku@gmail.com>
2023-01-24 17:14:28 +0900

    add scroll animation

--- app.js
+++ app.js
@@ -61,3 +61,22 @@ links.forEach(link => {
         checkbox.checked = false;
     });
 });
+
+
+
+
+// animation
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
